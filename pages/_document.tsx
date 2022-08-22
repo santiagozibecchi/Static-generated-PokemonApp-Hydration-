2c4922df29fb7e6071649dad6b114a05416f133b
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-22 14:41:43 -0300

    conf nextUI

---
+++ pages/_document.tsx
@@ -0,0 +1,49 @@
+import Document, {
+   Html,
+   Head,
+   Main,
+   NextScript,
+   DocumentContext,
+   DocumentInitialProps,
+} from "next/document";
+import { CssBaseline } from "@nextui-org/react";
+import React from "react";
+
+class MyDocument extends Document {
+   static async getInitialProps(
+      ctx: DocumentContext
+   ): Promise<DocumentInitialProps> {
+      const originalRenderPage = ctx.renderPage;
+
+      // Run the React rendering logic synchronously
+      ctx.renderPage = () =>
+         originalRenderPage({
+            // Useful for wrapping the whole react tree
+            enhanceApp: (App) => App,
+            // Useful for wrapping in a per-page basis
+            enhanceComponent: (Component) => Component,
+         });
+
+      // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+         ...initialProps,
+         styles: React.Children.toArray([initialProps.styles]),
+      };
+   }
+
+   render() {
+      return (
+         <Html lang="es">
+            <Head>{CssBaseline.flush()}</Head>
+            <body>
+               <Main />
+               <NextScript />
+            </body>
+         </Html>
+      );
+   }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 
 const HomePage: NextPage = () => {
-   return <h1>Hola mundo</h1>;
+   return (
+      <>
+         <Button color="gradient">Hola mundo</Button>
+      </>
+   );
 };
 
 export default HomePage;
