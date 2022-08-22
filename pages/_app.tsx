b5771bac1c962dcc2ba785ef56e0d07a1b75fa4e
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-22 14:56:12 -0300

    darkTheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
+import { darkTheme } from "../themes";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
    return (
-      <NextUIProvider>
-         <Component {...pageProps} />;
+      <NextUIProvider theme={darkTheme}>
+         <Component {...pageProps} />
       </NextUIProvider>
    );
 }
